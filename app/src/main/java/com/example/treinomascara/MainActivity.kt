405afd21e6373fa51a367ce681115c65f9e15a86
HelvioSiqueira <helviosiqueira@outlook.com>
2022-05-21 19:15:13 -0300

    commit comum

--- app/src/main/java/com/example/treinomascara/MainActivity.kt
+++ app/src/main/java/com/example/treinomascara/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.treinomascara
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.treinomascara.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,64 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.inputCelular.addTextChangedListener(object: TextWatcher{
+
+            val inputCelular = binding.inputCelular
+
+            var isUpdating = false
+
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(isUpdating){
+                    isUpdating = false
+                    return
+                }
+
+                val hasMask = s.toString().indexOf('(') > -1 || s.toString().indexOf(')') > -1 ||
+                        s.toString().indexOf(' ') > -1 ||
+                        s.toString().indexOf('-') > -1
+
+                var str = s.toString().filterNot { it == '(' || it == ')' || it == '-' || it == ' ' }
+
+                if(count > before){
+                    if(str.length > 7){
+                        str = "(${str.substring(0, 2)}) ${str.substring(2, 7)}-${str.substring(7)}"
+                    } else if(str.length > 3){
+                        str = "(${str.substring(0, 2)}) ${str.substring(2)}"
+                    } else if(str.length > 2){
+                        str = "(${str.substring(0, 2)}) ${str.substring(2)}"
+                    } else if(str.length > 1){
+                        str = "(${str.substring(0)})"
+                    }
+
+                    isUpdating = true
+                    inputCelular.setText(str)
+                    inputCelular.setSelection(inputCelular.text?.length ?: 0)
+                } else if (before > count){
+                    if(str.length > 8){
+                        str = "(${str.substring(0, 2)}) ${str.substring(2, 7)}-${str.substring(7)}"
+                    } else if(str.length < 7){
+                        str = "(${str.substring(0, 2)}) ${str.substring(0, 6)}"
+                    }  else if(str.length < 3){
+                        str = "(${str.substring(0, 2)}) "
+                    } else if(str.length < 2){
+                        str = "(${str.substring(0, 2)})"
+                    } else if(str.length < 1){
+                        str = "(${str.substring(0, 0)})"
+                    }
+
+                    isUpdating = true
+                    inputCelular.setText(str)
+                    inputCelular.setSelection(inputCelular.text?.length ?: 0)
+                } else {
+                    isUpdating = true
+                    inputCelular.setText(str)
+                    inputCelular.setSelection(Math.max(0,
+                        Math.min(if (hasMask) start - before else start, str.length)))
+                }
+            }
+        })
     }
 }
